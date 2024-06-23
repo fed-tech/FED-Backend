8ff6ec7d890cfc98b4f98a6f88bc90ea7a0698a5
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2024-06-23 23:47:14 +0530

    Updating form routes adding registrationcontroller

--- routes/forms/formRoutes.js
+++ routes/forms/formRoutes.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { addForm, editForm, deleteForm, getAllForms } = require('../../controllers/forms/formController'); //calling addForm, editForm, deleteForm, getAllForms functions from formController.js
+const formController = require('../../controllers/forms/formController')
+const registrationController = require('../../controllers/registration/registrationController');
 const { verifyToken } = require('../../middleware/verifyToken');
 const { isAdmin } = require('../../middleware/isAdmin');
 const { addFormValidationRules } = require('../../middleware/validator/addFormValidator'); // Ensure this path is correct
 const { validate } = require('../../middleware/validationMiddleware');
-const registrationController = require('../../controllers/registration/registrationController');
+
+router.use('/register', verifyToken, registrationController.addRegistration)
+router.get('/download/:id', verifyToken, registrationController.downloadRegistration)
 
 /**
  * @description Get all forms
  * @route       GET /api/forms/getAllForms
  * @access      Public
  */
-router.get('/getAllForms', getAllForms);
+router.get('/getAllForms', formController.getAllForms);
 
 // Apply verifyToken and isAdmin middleware globally to all routes in this router
 router.use(verifyToken, isAdmin);
@@ -23,20 +26,21 @@ router.use(verifyToken, isAdmin);
  * @access      Admin
  * @middleware  addFormValidationRules
  */
-router.post('/addForm', addFormValidationRules(), validate, addForm);
+router.post('/addForm', addFormValidationRules(), validate, formController.addForm);
 
 /**
  * @description Delete a form
  * @route       DELETE /api/forms/deleteForm/:id
  * @access      Admin
  */
-router.delete('/deleteForm/:id', validate, deleteForm);
+router.delete('/deleteForm/:id', validate, formController.deleteForm);
 
 /**
  * @description Edit a form
  * @route       PUT /api/forms/editForm/:id
  * @access      Admin
  */
-router.put('/editForm/:id', validate , editForm);
+router.put('/editForm/:id', validate , formController.editForm);
 
 module.exports = router;
+
