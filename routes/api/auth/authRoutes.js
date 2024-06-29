3cced25a29ee827b3ed3f92200b195547a9fceb4
Amaldeep <amaldeeppatra21@gmail.com>
2024-06-29 17:40:22 +0530

    google auth done

--- routes/api/auth/authRoutes.js
+++ routes/api/auth/authRoutes.js
@@ -2,12 +2,51 @@ const express = require('express');
 const router = express.Router();
 const { validate } = require('../../../middleware/validationMiddleware');
 const { registerValidationRules, loginValidationRules } = require('../../../middleware/validator/authValidator');
+require('dotenv').config();
+
+const passport = require("passport");
 
 // Import the auth controllers required
 const { login, verifyEmail, register, forgetPassword, changePassword, logout } = require('../../../controllers/auth/authController');
 
 // Import the middlewares required
 const { isUser } = require('../../../middleware/access/userAccess');
+
+router.get("/login/success", (req, res) => {
+    if(req.user){
+        res.status(200).json({
+            error: false,
+            message: "Successfully Logged In",
+            user: req.user,
+        });
+    } else{
+        res.status(403).json({error: true, message: "Not Authorized"})
+    }
+});
+
+router.get("/login/failed", (req, res) => {
+    res.status(401).json({
+        error: true,
+        message: "Log in failure",
+    });
+});
+
+router.get("/google", passport.authenticate('google', {scope:
+    ['email', 'profile']
+}));
+
+router.get(
+    "/google/callback",
+    passport.authenticate("google", {
+        successRedirect: process.env.CLIENT_URL,
+        failureRedirect: "/login/failed",
+    })
+);
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect(process.env.CLIENT_URL);
+});
 
 // Define the authentication routes here
 
