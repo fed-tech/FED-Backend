4db8f8561b6286e3cb3c2068c377d7bf0ef584df
Amaldeep <amaldeeppatra21@gmail.com>
2024-08-05 12:24:42 +0530

    google auth done

--- controllers/auth/google/googleAuthentication.js
+++ controllers/auth/google/googleAuthentication.js
@@ -23,22 +23,16 @@ const googleAuth = expressAsyncHandler(async(req, res) => {
         }
     }
     else{
-        
         const id = await bcrypt.hash(id, 10);
         data.password = id;
         data.access = AccessTypes.USER;
         const newUser = await createUser(data, sendMailFlag);
         const token = jwt.sign({ id: newUser.id, email, loginTime: new Date().toISOString() }, process.env.JWT_SECRET, { expiresIn: '7h' });
-        // Send the token as an HTTP-only cookie
         res.cookie('token', token, {
             httpOnly: true,
             secure: true
         });
-
-        // Delete the password before sending the new created user data 
         delete newUser.password;
-
-        // Respond to the client immediately
         res.status(201).json({ message: 'User created successfully', user : newUser, token: token });
     }
 })
